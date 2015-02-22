eae44fc24f5ac340f31b14764d5d0df7232ad6ad
Martin Gontovnikas <martin@gon.to>
2015-02-22 18:45:53 -0500

    Changed homepage

---
+++ js/site.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#header").midnight();
+  $(".scroll-down").on("click", function(){
+    $("html, body").animate({scrollTop: $("#services").offset().top}, "slow");
+  });
+  $(".backToTop").on("click", function(e){
+    $("html, body").animate({scrollTop: $("#hero").offset().top}, "slow");
+    e.preventDefault();
+  });
+});
