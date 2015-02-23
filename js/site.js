14ceae1c3c3e479aa94dd557d5d84cd47d14fdc6
Martin Gontovnikas <martin@gon.to>
2015-02-22 23:25:38 -0800

    Finished working on it

--- js/site.js
+++ js/site.js
@@ -1,3 +1,7 @@
+$("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+
+$("article").fitVids();
+
 $(document).ready(function() {
   $("#header").midnight();
 
@@ -9,6 +13,25 @@ $(document).ready(function() {
     $("html, body").animate({scrollTop: $("#hero").offset().top}, "slow");
     e.preventDefault();
   });
+
+  $('.image-popup').magnificPopup({
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
+    },
+    removalDelay: 300, // Delay in milliseconds before popup is removed
+    // Class that is added to body when popup is open.
+    // make it unique to apply your CSS animations just to this exact popup
+    mainClass: 'mfp-fade'
+  });
+
+
 
 
 });
